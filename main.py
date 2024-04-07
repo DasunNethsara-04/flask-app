2a094956f958dce104e7bde2ca947f13183a8608
Dasun <techsaralk.pro@gmail.com>
2024-04-07 12:52:43 +0530

    Login and registration pages added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# imports
 from flask import Flask, render_template
 
+# initialize the flask app
 app: Flask = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +15,15 @@ def about_us():
 @app.route("/contact")
 def contact_us():
     return render_template("Contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("Login.html")
+
+@app.route("/register")
+def register():
+    return render_template("Register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
