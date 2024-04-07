4b315d4f25c5d3882bceadf0fc99b2f1a77e26a4
Dasun <techsaralk.pro@gmail.com>
2024-04-07 12:16:57 +0530

    about us, contact routings added

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app: Flask = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about_us():
+    return render_template("AboutUs.html")
+
+@app.route("/contact")
+def contact_us():
+    return render_template("Contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
