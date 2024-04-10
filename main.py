a110b5410072f369f1bbcc8bfc589f58e3f8c706
Dasun <techsaralk.pro@gmail.com>
2024-04-11 00:29:14 +0530

    404 page added and finised

--- main.py
+++ main.py
@@ -106,5 +106,9 @@ def adminPage():
 def admin_dashboard():
     return render_template("Pages/dashboard.html")
 
+@app.errorhandler(404)
+def page_not_found(err):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
